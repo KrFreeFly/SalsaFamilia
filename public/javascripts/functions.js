1fa4640f041058022251fde537628cf8c616cb46
KrFreeFly <mikesplace@mail.ru>
2021-10-03 22:14:28 +0300

    Promise for query creation is in functions now

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -14,3 +14,18 @@ exports.query = function (sqlQuery, arg) {
         });
     });
 };
+
+//подгружаем драйвер mySQL
+const mysql = require("mysql2");
+
+//создаем пул подключений к базе данных
+const pool = mysql.createPool({
+    connectionLimit: 5,
+    host: "sql11.freemysqlhosting.net",
+    user: "sql11440439",
+    database: "sql11440439",
+    password: "9XLN1AfatB"
+});
+
+exports.mysql
+exports.pool
