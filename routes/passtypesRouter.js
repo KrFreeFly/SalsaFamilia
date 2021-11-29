b8c44dfff41d500549307a1767fa582a3f41fefe
KrFreeFly <mikesplace@mail.ru>
2021-11-29 14:06:24 +0300

    Passtypes page finished

--- routes/passtypesRouter.js
+++ routes/passtypesRouter.js
@@ -5,11 +5,9 @@ const passtypesRouter = express.Router();
 const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
-passtypesRouter.use("/newpasstype", passtypesController.newPasstype);
-passtypesRouter.use("/create", urlencodedParser, passtypesController.createPasstype);
-passtypesRouter.use("/update", urlencodedParser, passtypesController.updatePasstype);
-passtypesRouter.use("/delete:idPassType", urlencodedParser, passtypesController.deletePasstype);
-passtypesRouter.use("/:idPassType", passtypesController.getPasstype);
+passtypesRouter.post("/", urlencodedParser, passtypesController.createPasstype);
+passtypesRouter.put("/:idPassType", urlencodedParser, passtypesController.updatePasstype);
+passtypesRouter.delete("/:idPassType", urlencodedParser, passtypesController.deletePasstype);
 passtypesRouter.get("/", passtypesController.getPasstypes);
 
 module.exports = passtypesRouter;
